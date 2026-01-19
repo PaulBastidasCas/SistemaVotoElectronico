aeb6d25d22dddb57c94ccce92fba0e6a0f007155
Kevin-1224 <kapupialess@utn.edu.ec>
2026-01-19 07:18:58 -0500

    nuevos modelos

---
+++ SistemaVotoElectronico.Modelos/JefeDeMesa.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaVotoElectronico.Modelos
+{
+    public class JefeDeMesa
+    {
+        [Key] public int Id { get; set; }
+        public string Cedula { get; set; }
+        public string NombreCompleto { get; set; }
+        public string Correo { get; set; }
+        public string Contrasena { get; set; }
+        public string Fotografia { get; set; }
+
+        public string Rol { get; set; } = "JefeDeMesa";
+        public Mesa? MesaAsignada { get; set; }
+        public List<PadronElectoral> HistorialVotos { get; set; } = new List<PadronElectoral>();
+    }
+}
--- SistemaVotoElectronico.Modelos/Mesa.cs
+++ SistemaVotoElectronico.Modelos/Mesa.cs
@@ -8,20 +8,18 @@ using System.Threading.Tasks;
 
 namespace SistemaVotoElectronico.Modelos
 {
-    public class ListaElectoral
+    public class Mesa
     {
         [Key] public int Id { get; set; }
         public string Nombre { get; set; }
-        public string NumeroLista { get; set; }
-        public string Siglas { get; set; }
-        public string Logotipo { get; set; }
-        public string Color { get; set; }
+        public string Ubicacion { get; set; }
 
         public int EleccionId { get; set; }
-
         [ForeignKey("EleccionId")]
         public Eleccion? Eleccion { get; set; }
 
-        public List<Candidato>? Candidatos { get; set; } = new List<Candidato>();
+        public int? JefeDeMesaId { get; set; }
+        [ForeignKey("JefeDeMesaId")]
+        public JefeDeMesa? JefeDeMesa { get; set; }
     }
 }
